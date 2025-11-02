aec38668e8657322c62faf3e8d50cf7987435e47
akhareakshay <akhareakshay2@gmail.com>
2025-11-02 14:50:31 +0530

    get min and max numbers

---
+++ src/com/codewithakshay/HFindMinAndMaxNumbers.java
@@ -0,0 +1,26 @@
+package com.codewithakshay;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+/*8) Given a list of integers, find maximum and minimum of those numbers?*/
+
+public class HFindMinAndMaxNumbers {
+
+	public static void main(String[] args) {
+		
+		List<Integer> listOfIntegers = Arrays.asList(45, 12, 56, 15, 24, 75, 31, 89);
+		
+		Integer minNumber = listOfIntegers.stream()
+								.min(Comparator.naturalOrder()).get();
+		System.out.println("min number : "+minNumber);
+		
+		Integer maxNumber = listOfIntegers.stream()
+							.max(Comparator.naturalOrder()).get();
+		System.out.println("max number : "+maxNumber);
+		
+	}
+	
+}
