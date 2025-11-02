6baeff39e6d05ae7608aead0d010c4c72b9f2751
akhareakshay <akhareakshay2@gmail.com>
2025-11-02 16:13:20 +0530

    sort string based on their length

---
+++ src/com/codewithakshay/OSortStringsBasedOnTheirLength.java
@@ -0,0 +1,21 @@
+package com.codewithakshay;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/*15) Given a list of strings, sort them according to increasing order of their length?*/
+
+public class OSortStringsBasedOnTheirLength {
+
+	public static void main(String[] args) {
+		
+		List<String> listOfStrings = Arrays.asList("Java", "Python", "C#", "HTML", "Kotlin", "C++", "COBOL", "C");
+		
+		listOfStrings.stream()
+							.sorted(Comparator.comparing(String::length))
+							.forEach(System.out::println);
+		
+	}
+	
+}
