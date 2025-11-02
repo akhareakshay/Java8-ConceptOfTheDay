13f926bb15b9b3144b81e9470765ff35f5c71b88
akhareakshay <akhareakshay2@gmail.com>
2025-11-02 16:04:00 +0530

    find second largest number

---
+++ src/com/codewithakshay/NFindSecondLargestNumber.java
@@ -0,0 +1,25 @@
+package com.codewithakshay;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+/*14) Find second largest number in an integer array?*/
+
+public class NFindSecondLargestNumber {
+
+	public static void main(String[] args) {
+		
+		List<Integer> listOfIntegers = Arrays.asList(45, 12, 56, 15, 24, 75, 31, 89);
+		
+		int secondLargest = listOfIntegers.stream()
+							.sorted(Comparator.reverseOrder())
+							.skip(1)
+							.findFirst().get();
+		
+		System.out.println("second largest number : "+secondLargest);
+		
+	}
+	
+}
