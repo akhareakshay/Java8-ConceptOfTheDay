e9943bd214132f99f6c04b3687c506e59aa98d8f
akhareakshay <akhareakshay2@gmail.com>
2025-11-02 15:01:18 +0530

    merge two arrays into single sorted array without duplicates

---
+++ src/com/codewithakshay/JMergeTwoUnsortedArraysIntoSingleSortedArrayWithoutDuplicate.java
@@ -0,0 +1,24 @@
+package com.codewithakshay;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+/*10) How do you merge two unsorted arrays into single sorted array without duplicates?*/
+
+public class JMergeTwoUnsortedArraysIntoSingleSortedArrayWithoutDuplicate {
+
+	public static void main(String[] args) {
+		
+		int[] a = new int[] {4, 2, 5, 1};
+        
+        int[] b = new int[] {8, 1, 9, 5};
+        
+        int[] mergedSortedArrayWithoutDuplicates = IntStream.concat(Arrays.stream(a), Arrays.stream(b))
+        							.sorted()
+        							.distinct()
+        							.toArray();
+        System.out.println(Arrays.toString(mergedSortedArrayWithoutDuplicates));
+		
+	}
+	
+}
